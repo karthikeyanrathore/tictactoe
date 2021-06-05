3f1c2d0fd1e1535855833f3a8d091ecae92e71a6
karthikeyanrathore <karthikerathore@gmail.com>
2021-06-06 00:04:22 +0530

    frame

---
+++ src/frame.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import cv2 as cv
+
+def frame():
+  cap = cv.VideoCapture(0)
+  if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+  while(1):
+    ret, frame = cap.read()
+    if not ret:
+      print("Can't receive frame (stream end?). Exiting ...")
+      break
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    cv.imshow('frame', gray)
+    if (cv.waitKey(1) == ord('q')):
+      break
+
+  cap.release()
+  cv.destroyAllWindows()
+
+if __name__ == "__main__":
+  frame()
